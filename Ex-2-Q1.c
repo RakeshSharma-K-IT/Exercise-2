bebbdd78fc202facf3a7903c1dd6241ec6679862
RakeshSharma-K-IT <rkshsharmak@gmail.com>
2025-02-07 12:41:53 +0530

    Add files via upload

---
+++ Ex-2-Q1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define pi 3.14
+int main()
+{
+    float r;
+    printf("Enter the radius of the circle =");
+    scanf("%f",&r);
+    printf("The area of circcle= %.2f",pi*r*r);
+}
