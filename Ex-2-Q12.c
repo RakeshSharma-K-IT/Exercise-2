fdfec2b242e1129aab18266fbf26815ef0b8ef6e
RakeshSharma-K-IT <rkshsharmak@gmail.com>
2025-02-07 12:58:41 +0530

    Add files via upload

---
+++ Ex-2-Q12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number =");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("It is even number ");
+    }
+    else 
+    {
+        printf("It is odd number");
+    }
+}
